0cba1629cf6c2672dd9c98967c6e4a2c166df60e
raymond.hung <raymond.hung>
2016-08-12 12:24:44 +0800

    add popup video

---
+++ eggshell_Q3/js/video.js
@@ -0,0 +1,16 @@
+function videoPopup(videoId) {
+
+    $('#youtube-iframe').attr('src', 'http://www.youtube.com/embed/' + videoId + '?rel=0&autoplay=1');
+    $('.popup-video').css('display', 'block');
+}
+
+$(document).ready(function() {
+
+    var videoId = 'hWhFgcOMioQ';
+    videoPopup(videoId);
+
+    $('.popup-video-close').click(function() {
+        $('#youtube-iframe').attr('src', '');
+        $('.popup-video').css('display', 'none');
+    });
+});
